f8d2345ac2141c2baa1068ddb9cd8714cd81d553
MairinK <mairinkavanagh@gmail.com>
2013-04-10 10:46:41 +0100

    bug123

---
+++ gitlab/src/ie/gcd/griffith/Sample.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+/**
+ * @author MairinK
+ *
+ */
+package ie.gcd.griffith;
+
+public class Sample{
+	
+	public Sample(){
+		
+	
+	
+}
+
+public static void main(String[] args){ 
+	
+}
+}
